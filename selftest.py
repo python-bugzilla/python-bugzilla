e6f699f06e92b1e49b1b8d2c8fbe89d9425a4a9a
Will Woods <wwoods@wills-mini.localdomain>
2007-09-04 14:47:59 -0400

    Add GPL header to selftest.py

--- selftest.py
+++ selftest.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 # Simple self-test of the bugzilla module
 
+# Copyright (C) 2007 Red Hat Inc.
+# Author: Will Woods <wwoods@redhat.com>
+# 
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.  See http://www.gnu.org/copyleft/gpl.html for
+# the full text of the license.
+
 from bugzilla import Bugzilla
 import os, glob, sys
 
