8dd185ed847b6ce7d77ea1368c76654d9dda2edc
Will Woods <wwoods@redhat.com>
2007-11-05 16:53:12 -0500

    fix some packaging junk, clarify README a bit

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 from glob import glob
 import bugzilla
 
-setup(name='bugzilla',
+setup(name='python-bugzilla',
       version=str(bugzilla.version),
       description='Bugzilla XMLRPC access module',
       author='Will Woods',
       author_email='wwoods@redhat.com',
-      url='http://localhost/',
+      url='http://wwoods.fedorapeople.org/python-bugzilla/',
       py_modules=['bugzilla'],
       scripts=['bugzilla'],
 )
