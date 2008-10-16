1d06d40952309044ef7e5f5dd728022c77d68549
Will Woods <wwoods@redhat.com>
2008-10-16 16:14:28 -0400

    Update version for rc4.

--- bugzilla/base.py
+++ bugzilla/base.py
@@ -18,7 +18,7 @@ import os.path, base64
 import logging
 log = logging.getLogger('bugzilla')
 
-version = '0.4-rc3'
+version = '0.4-rc4'
 user_agent = 'Python-urllib2/%s bugzilla.py/%s' % \
         (urllib2.__version__,version)
 
