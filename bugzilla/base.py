24463a5550a6a449134b1868e4960d28ddca338c
Will Woods <wwoods@redhat.com>
2009-03-25 12:45:59 -0400

    I declare 0.5 final! Mark it, dude!

--- bugzilla/base.py
+++ bugzilla/base.py
@@ -21,7 +21,7 @@ import logging
 
 log = logging.getLogger('bugzilla')
 
-version = '0.5-rc1'
+version = '0.5'
 user_agent = 'Python-urllib2/%s bugzilla.py/%s' % \
         (urllib2.__version__,version)
 
